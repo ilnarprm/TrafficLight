3806f2b190a0ca2d1d6ff99050ed681c20f85a50
ilnarprm <ilnar_prm@icloud.com>
2024-01-28 14:17:32 +0500

    working app

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,22 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    @IBOutlet var RedView: UIView!
+    @IBOutlet var redLabel: UIView!
     
-    @IBOutlet var YelowView: UIView!
+    @IBOutlet var yellowLabel: UIView!
     
-    @IBOutlet var GreenView: UIView!
+    @IBOutlet var greenLabel: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //redLabel.alpha = 0.5
+        redLabel.layer.cornerRadius = 50
+        yellowLabel.layer.cornerRadius = 50
+        greenLabel.layer.cornerRadius = 50
+        var count = 1
+        
+        
     }
     
-    @IBAction func NextButton(_ sender: UIButton) {
+    @IBAction func startButton(_ sender: UIButton) {
+        sender.setTitle("NEXT", for: .normal)
+        if redLabel.alpha == 0.5 && yellowLabel.alpha == 0.5 && greenLabel.alpha == 0.5 {
+            redLabel.alpha = 1
+        } else if redLabel.alpha == 1 {
+            redLabel.alpha = 0.5
+            yellowLabel.alpha = 1
+        } else if yellowLabel.alpha == 1 {
+            yellowLabel.alpha = 0.5
+            greenLabel.alpha = 1
+        } else if greenLabel.alpha == 1 {
+            greenLabel.alpha = 0.5
+            redLabel.alpha = 1
+        }
+        
     }
-    
-
 }
 
